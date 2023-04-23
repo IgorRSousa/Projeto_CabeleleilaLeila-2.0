90e3be4a118f8c29cf781b07241e303ee5c12731
Igor Sousa <igorrafaelsb248@gmail.com>
2023-04-23 16:42:22 -0300

    Efetuado a logica para bloquear edição do agendamento com menos de dois dias para o prazo

--- views/editarAgendamento.php
+++ views/editarAgendamento.php
@@ -1,17 +1,24 @@
 <?php
+
+use Illuminate\Support\Facades\Date;
+
     $url = 'http://localhost:8000/consultarAgendamento/'.$_POST["id"].'';
     $dado = file_get_contents($url); // executa link da api e recebe a resposta
     $jsonDados = json_decode($dado);
+
+    $data_agendamento = new Datetime($jsonDados->data);
+    $data_atual = new Datetime(date('Y-m-d'));
+    $diferenca = $data_agendamento->diff($data_atual);
 ?>
 
 <h1 class="m-3 text-center">Editar Serviço</h1>
-<form action="salvarAlteracaoAgendamento" method="post">
+<form action="<?php if($diferenca->d <= 2){print 'listarAgenda';}else{print 'salvarAlteracaoAgendamento';}?>" method="post">
 <input type="hidden" name="id" value="<?php print $_POST["id"]?>">
     <div class="row mx-5 mb-3">
         <div class="col"></div>
         <div class="col-sm-6">  
             <label>Nome</label>
-            <input type="text" name="nome" class="form-control"  placeholder="Nome"  value="<?php print $jsonDados->nome?>" required>
+            <input type="text" name="nome" class="form-control"  placeholder="Nome"  value="<?php print $jsonDados->nome?>" <?php if($diferenca->d <= 2){print 'disabled=""';}?>required>
         </div>
         <div class="col"></div>
     </div>
@@ -20,7 +27,7 @@
         <div class="col"></div>
         <div class="col-sm-6">
             <label>Serviço desejado</label>
-            <input type="text" name="servico" class="form-control"  placeholder="Serviço desejado" value="<?php print $jsonDados->servico?>" required>
+            <input type="text" name="servico" class="form-control"  placeholder="Serviço desejado" value="<?php print $jsonDados->servico?>" <?php if($diferenca->d <= 2){print 'disabled=""';}?> required>
         </div>
         <div class="col"></div>
     </div>
@@ -29,11 +36,11 @@
         <div class="col"></div>
         <div class="col-sm-3">
             <label>Data do Agendamento</label>
-            <input type="date" name="data" class="form-control"  value="<?php print $jsonDados->data?>" required>
+            <input type="date" name="data" class="form-control"  value="<?php print $jsonDados->data?>" <?php if($diferenca->d <= 2){print 'disabled=""';}?> required>
         </div>
         <div class="col-sm-3">
             <label>Horario do Agendamento</label>
-            <input type="time" name="hora" class="form-control" value="<?php print $jsonDados->hora?>" required>
+            <input type="time" name="hora" class="form-control" value="<?php print $jsonDados->hora?>" <?php if($diferenca->d <= 2){print 'disabled=""';}?> required>
         </div>
         <div class="col"></div>
     </div>
@@ -42,15 +49,31 @@
         <div class="col sm"></div>
         <div class="col-sm-6">
             <label>Observação</label>
-            <input type="text" name="observacao" class="form-control" placeholder="Observação" value="<?php print $jsonDados->observacao?>">
-        </div>  
+            <input type="text" name="observacao" class="form-control" placeholder="Observação" value="<?php print $jsonDados->observacao?>" <?php if($diferenca->d <= 2){print 'disabled=""';}?>>
+        </div> 
         <div class="col"></div>
     </div>
 
+    <?php
+    if($diferenca->d <= 2){ 
+        print'<div class="row mx-5 mb-3">
+            <div class="col sm"></div>
+                <div id="emailHelp" class="form-text col-sm-6">Menos de dois dias para o Serviço caso queira reagendar ligar para (14) 1234-5678.</div>
+            <div class="col"></div>
+        </div>';
+    }
+    ?>
+    
     <div class="row mx-5">
             <div class="col-sm-3"></div>
             <div class="col">
-                <button type="submit" class="btn btn-primary">Enviar</button>
+            <?php 
+            if($diferenca->d <= 2){
+                print '<button type="submit" class="btn btn-primary">Voltar</button>';
+            }else{
+                print '<button type="submit" class="btn btn-primary">Enviar</button>';
+            }?>
             </div>
+            
     </div>
 </form>
